e3bab1026d8ebc3bb4ded2400103be4af8c18967
SharadPatel1993 <sharad182@hotmail.com>
2018-05-07 11:50:56 +0100

    Code the Factory pattern

---
+++ DesignPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new SavingsAccountFactory() as ICreditUnionFactory;
+            var citiAccount = factory.GetSavingsAccount("CITI-321");
+            var nationalAccount = factory.GetSavingsAccount("NATIONAL-987");
+
+            Console.WriteLine($"My citi balance is ${citiAccount.Balance} and national balance is ${nationalAccount.Balance}");
+        }
+    }
+
+    // Product
+    public abstract class ISavingsAccount
+    {
+        public decimal Balance { get; set; }
+    }
+
+    // Concrete Product
+    public class CitiSavingsAccount : ISavingsAccount
+    {
+        public CitiSavingsAccount()
+        {
+            Balance = 5000;
+        }
+    }
+
+    // Concrete Product
+    public class NationalSavingsAccount : ISavingsAccount
+    {
+        public NationalSavingsAccount()
+        {
+            Balance = 2000;
+        }
+    }
+
+    // Creator
+    interface ICreditUnionFactory
+    {
+        ISavingsAccount GetSavingsAccount(string acctNo);
+    }
+
+    // Concrete Creators
+    public class SavingsAccountFactory : ICreditUnionFactory
+    {
+        public ISavingsAccount GetSavingsAccount(string acctNo)
+        {
+            if (acctNo.Contains("CITI")) { return new CitiSavingsAccount(); }
+            else if (acctNo.Contains("NATIONAL")) { return new NationalSavingsAccount(); }
+            else { throw new ArgumentException("Invalid Account Number"); }
+        }
+    }
+}
